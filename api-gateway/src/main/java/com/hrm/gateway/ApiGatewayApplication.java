42d72de99420606e036ab1c460b57e34381d5223
PHOENIX\USER <phungphan@wwct.vn>
2025-12-01 16:50:34 +0700

    Initial commit

---
+++ api-gateway/src/main/java/com/hrm/gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hrm.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient // Bật khả năng Client của Eureka
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
